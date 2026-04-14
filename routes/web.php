700ade4f34b4c928c9b517219fdd810e0ea1f3a8
hani06465 <hanikiros042@gmail.com>
2026-04-14 09:10:20 +0300

    i integrated login system and make multi-authentication system for it

--- routes/web.php
+++ routes/web.php
@@ -9,11 +9,21 @@ Route::get('/', function () {
 });
 
 // the rout for Admin
+
 //Route::resource('admin/dashboard',AdminController::class)->only(['index']);  // we updated this with ..
 
 Route::prefix('admin')->group(function () {
+    //show login from
+    Route::get('login', [AdminController::class, 'create'])->name('admin.login');
     //Dashboard route
-    Route::resource('dashboard', AdminController::class)->only(['index']);
+    // i avoid this usage and i enserted this beacuse i want first to loging the admin before access it or it restricts from direct acess.
+   //Route::resource('dashboard', AdminController::class)->only(['index']);
+   // here now we can access this dashboard after we 
+   Route::group(['middleware' => ['admin']],function() {
+    //Dashboard
+    // we then enter the dashboard route to this beacause we need first to  loginig first.
+    Route::resource('dashboard',AdminController::class)->only(['index']);
+   });
 });
 
 
